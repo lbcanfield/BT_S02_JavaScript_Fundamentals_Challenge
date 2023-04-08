9f816d4f7752959cd2b7f70ecf1597009cd75905
lbcanfield <lewie.b.canfield@gmail.com>
2023-04-07 23:06:58 -0500

    CuboidMaker constructor and methods created.

--- index.js
+++ index.js
@@ -195,8 +195,10 @@ function greeting(fName, lName) {
 - Instances of CuboidMaker should initialize `length`, `width` and `height` properties
 */
 
-function CuboidMaker(/*Your Code Here */) {
-     /*Your Code Here */
+function CuboidMaker(shapeObj) {
+     this.length = shapeObj.length;
+     this.width = shapeObj.width;
+     this.height = shapeObj.height;
 }
 
 
@@ -205,6 +207,9 @@ function CuboidMaker(/*Your Code Here */) {
   💡 NOTE: Formula for cuboid volume: length * width * height   
 */
 
+CuboidMaker.prototype.volume = function () {
+     return this.length * this.width * this.height;
+}
 
 
 
@@ -213,14 +218,16 @@ function CuboidMaker(/*Your Code Here */) {
   💡 NOTE: Formula for cuboid surface area: 2 * (length * width + length * height + width * height)  
 */
 
-
+CuboidMaker.prototype.surfaceArea = function () {
+     return 2 * (this.length * this.width + this.length * this.height + this.width * this.height)
+}
 
 
 /* 🐴🐴🐴 Step 4: Create a new object that uses CuboidMaker (not auto graded)🐴🐴🐴
   Create an object called cuboid that uses the new keyword to use our CuboidMaker constructor
   Add properties and values of length: 4, width: 5, and height: 5 to cuboid. */
 
-
+const cuboid = new CuboidMaker({ length: 4, width: 5, height: 5 })
 
 
 
